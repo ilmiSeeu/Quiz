1d51de336998b1659668a7f5e4999a76838875e0
ilmiSeeu <119293718+ilmiSeeu@users.noreply.github.com>
2023-01-16 20:29:13 +0100

    Delete Quiz_Project directory

--- Quiz_Project/src/main/java/com/example/Quiz_Project/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Quiz_Project.repository;
-
-import com.example.Quiz_Project.pojo.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
--- Quiz_Project/src/main/java/com/example/Quiz_Project/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Quiz_Project.repository;
-
-import com.example.Quiz_Project.pojo.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
