3c178d643ae917c9e2a763c2e9262b0f947e40a0
ilmiSeeu <119293718+ilmiSeeu@users.noreply.github.com>
2023-01-16 19:26:28 +0100

    Delete Source directory

--- Source/src/main/java/com/example/javapro/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.javapro.repository;
-
-import com.example.javapro.pojo.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
